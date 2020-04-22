8ecc5f7237a8f1b70b3921c10e766088d8801219
charles <charleswm@gmail.com>
2020-04-22 17:48:19 +1200

    add drop down animation

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,14 @@ import 'svg-url-loader?iesafe!../assets/dis-logo.svg';
 import 'svg-url-loader?iesafe!../assets/discord.svg';
 
 console.log("Welcome! Greetings from app.js");
+
+document.addEventListener('DOMContentLoaded', function () {
+  let develop = document.querySelector('#develop');
+
+  develop.addEventListener('mouseenter', function () {
+    develop.lastElementChild.classList.add('gone');
+  });
+  develop.addEventListener('mouseleave', function () {
+    develop.lastElementChild.classList.remove('gone');
+  });
+});
