a34701ca9a8c30bfda0446ed8c10a901ff095700
charles <charleswm@gmail.com>
2020-04-27 16:07:13 +1200

    Added burger icon animation

--- src/js/app.js
+++ src/js/app.js
@@ -4,18 +4,22 @@ import '../css/app.css';
 console.log("Welcome! Greetings from app.js");
 
 document.addEventListener('DOMContentLoaded', function () {
-  const menu_item = document.querySelectorAll('.nav-list-menu');
 
+  // Nav menu drop down - adds and removes the 'active' class
+
+  const menu_item = document.querySelectorAll('.nav-list-menu');
   menu_item.forEach((item) => {
     const dropdown = item.lastElementChild;
-
     item.addEventListener('mouseenter', function () {
       dropdown.classList.add('active');
     });
-
     item.addEventListener('mouseleave', function () {
       dropdown.classList.remove('active');
     });
-
   });
+
+//  Mobile nav menu drop down and burger animation
+
+
+
 });
